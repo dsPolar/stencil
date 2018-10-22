cb373c9984c3d8b8965f8bf5a549c3fa09949aa7
dspolar <ds16797@my.bristol.ac.uk>
2018-10-22 11:00:28 +0100

    swapped i,j stencil for loop

--- stencil.c
+++ stencil.c
@@ -49,9 +49,10 @@ int main(int argc, char *argv[]) {
   free(image);
 }
 
+//called 200 times
 void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
-  for (int j = 0; j < ny; ++j) {
-    for (int i = 0; i < nx; ++i) {
+  for (int i = 0; i < nx; ++i) {
+    for (int j = 0; j < ny; ++j) {
       tmp_image[j+i*ny] = image[j+i*ny] * 3.0/5.0;
       if (i > 0)    tmp_image[j+i*ny] += image[j  +(i-1)*ny] * 0.5/5.0;
       if (i < nx-1) tmp_image[j+i*ny] += image[j  +(i+1)*ny] * 0.5/5.0;
