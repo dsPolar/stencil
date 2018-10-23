5f355180acfd1b406d6167456f28a8a9fdb6f668
dspolar <ds16797@my.bristol.ac.uk>
2018-10-23 12:42:57 +0100

    maybe working?

--- stencil.c
+++ stencil.c
@@ -104,15 +104,19 @@ void stencil(const int nx, const int ny, double * restrict image, double * restr
 
   //main body square
   //need to figure out cycle
+
   for (int z = nx+1; z<(max-nx)-1; z++){
     tmp_image[z] = image[z] *0.6;
     tmp_image[z] += image[z-nx] *0.1;
     tmp_image[z] += image[z-1] *0.1;
     tmp_image[z] += image[z+1] *0.1;
     tmp_image[z] += image[z+nx] *0.1;
+    if(z%nx == (nx-2)){
+      z+=2;
+    }
   }
 
-
+  /*
   for (int i = 0; i < nx; ++i) {
     for (int j = 0; j < ny; ++j) {
       tmp_image[j+i*nx] = image[j+i*nx] * 0.6;
@@ -122,7 +126,7 @@ void stencil(const int nx, const int ny, double * restrict image, double * restr
       if (j < nx-1) tmp_image[j+i*nx] += image[j+1+i*nx] * 0.1;
     }
   }
-}
+}*/
 /*
 void stencil(const int nx, const int ny, double * image, double * tmp_image) {
   const int max = nx*ny;
